505363a2880891833839c6a573aa47acdb7b48d7
Lina Stepanauskaite <lina.stepanauskaite@gmail.com>
2023-06-21 12:26:07 +0200

    updated shiny script

---
+++ Run_app.R
@@ -0,0 +1,3 @@
+source("shiny_UI_and_server.R")
+
+shinyApp(ui = ui, server = server)
--- shiny_UI_and_server.R
+++ shiny_UI_and_server.R
@@ -1,24 +1,56 @@
-remotes::install_github("Linastep-bot/projectMaster")
+#evtools::install_github('Linastep-bot/projectMaster')
 library(projectMaster)
-package_manager(c("shiny", "DESeq2"))
+package_manager(c('shiny', 'DESeq2', 'shinyjs'))
+
+options(shiny.maxRequestSize=100*1024^2)
 
 ui <- fluidPage(
-  titlePanel("DESeq2 Analysis"),
+  navbarPage("NO",
+  tabPanel("DESeq2 from count matrix", 
   sidebarLayout(
     sidebarPanel(
-      fileInput("countMatrix", "Upload Count Matrix", accept = ".csv"),
-      fileInput("designMatrix", "Upload Design Matrix", accept = ".csv"),
-      checkboxGroupInput("individuals", "Select Individuals", choices = NULL),
+      fileInput("countMatrix", "Upload Count Matrix", accept = c(".csv", ".txt")),
+      fileInput("designMatrix", "Upload Design Matrix", accept = c(".csv", ".txt")),
       actionButton("runAnalysis", "Run DESeq2")
     ),
+    
     mainPanel(
-      textOutput("results")
+      textOutput("results"))
     )
+  ),
+
+    tabPanel("DESeq2 from salmon files", 
+    sidebarLayout(
+      sidebarPanel(
+        fileInput("salmon_files", "Upload Salmon Files", multiple = TRUE, accept = ".sf"),
+        fileInput("metadata", "Upload Metadata", accept = c(".xlsx", ".txt", "xls", ".csv")),
+        fileInput("gff", "Upload GFF", accept = c(".gff", ".gff3")),
+        fileInput("fasta", "Upload Reference Genome", accept = c(".fa", ".fasta")),
+        actionButton("runAnalysis2", "Run DESeq2")
+      ),
+    
+    mainPanel(
+      textOutput("results"))
+      )
+    ),
+  
+  tabPanel("Cytoscape",
+             fluidRow(
+               column(
+                 width = 12,
+                 tags$iframe(src = "https://www.ndexbio.org/iquery/", width = "100%", height = "950px")
+               )
+             )
+           
+           )
+  
   )
 )
 
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+
   observeEvent(input$countMatrix, {
     # Read the count matrix file and extract individual names
     countMatrix <- read.csv(input$countMatrix$datapath)
@@ -28,7 +60,33 @@ server <- function(input, output) {
     updateCheckboxGroupInput(session, "individuals", choices = individuals)
   })
   
+  
   observeEvent(input$runAnalysis, {
+    # Read the count matrix and design matrix files
+    countMatrix <- read.csv(input$countMatrix$datapath)
+    designMatrix <- read.csv(input$designMatrix$datapath)
+    
+    # Extract the selected individuals
+    selectedIndividuals <- input$individuals
+    
+    # Subset the count matrix to include only selected individuals
+    countMatrixSubset <- countMatrix[, selectedIndividuals]
+    
+    # Perform DESeq2 analysis
+    dds <- DESeqDataSetFromMatrix(countData = countMatrixSubset, colData = designMatrix, design = ~ condition)
+    dds <- DESeq(dds)
+    res <- results(dds)
+    
+    # Display the results
+    output$results <- renderText({
+      # Format and display the differential expression results
+      # Customize this based on your specific needs
+      paste("Differential Expression Results:")
+      head(res)
+    })
+  })
+  
+  observeEvent(input$runAnalysis2, {
     # Read the count matrix and design matrix files
     countMatrix <- read.csv(input$countMatrix$datapath)
     designMatrix <- read.csv(input$designMatrix$datapath)
@@ -59,63 +117,35 @@ shinyApp(ui = ui, server = server)
 
 
 
-#my edits
-ui <- fluidPage(
-  titlePanel("DESeq2 Analysis"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("salmon_files", "Upload Salmon Files", multiple = TRUE, accept = ".sf"),
-      fileInput("salmon_files", "Upload Metadata", accept = c(".xlsx", ".txt", "xls", ".csv")),
-      fileInput("gff", "Upload GFF", accept = c(".gff", ".gff3")),
-      fileInput("fasta", "Upload Reference Genome", accept = c(".fa", ".fasta")),
-      checkboxGroupInput("individuals", "Select Individuals", choices = NULL),
-      actionButton("runAnalysis", "Run DESeq2")
-    ),
-    mainPanel(
-      textOutput("results")
-    )
-  )
-)
 
 
-server <- function(input, output) {
-  observeEvent(input$countMatrix, {
-    # Read the count matrix file and extract individual names
-    
-    countMatrix <- read.csv(input$countMatrix$datapath)
-    individuals <- colnames(countMatrix)
-    
-    # Update the choices of the checkbox group
-    updateCheckboxGroupInput(session, "individuals", choices = individuals)
-  })
-  
-  observeEvent(input$runAnalysis, {
-    # Read the count matrix and design matrix files
-    countMatrix <- read.csv(input$countMatrix$datapath)
-    designMatrix <- read.csv(input$designMatrix$datapath)
-    
-    # Extract the selected individuals
-    selectedIndividuals <- input$individuals
-    
-    # Subset the count matrix to include only selected individuals
-    countMatrixSubset <- countMatrix[, selectedIndividuals]
-    
-    #Filtering matrix
-    
-    # Perform DESeq2 analysis
-    dds <- DESeqDataSetFromMatrix(countData = countMatrixSubset, colData = designMatrix, design = ~ condition)
-    dds <- DESeq(dds)
-    res <- results(dds)
-    
-    # Display the results
-    output$results <- renderText({
-      # Format and display the differential expression results
-      # Customize this based on your specific needs
-      paste("Differential Expression Results:")
-      head(res)
-    })
-  })
-}
 
 
-shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
